859f6be9ffef5713379050f478b2822ea722b871
karthikeyan <karthikeyan9108354@gmail.com>
2026-04-10 13:33:36 +0530

    Allow all hosts in Vite preview to prevent Render blocking

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['preethi-portflio.onrender.com']
+    allowedHosts: true
   }
 })
